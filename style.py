95ec666462bf7f4ca81b004ebe7b28e0edeab681
danieljabbour <jabbourd@mcmaster.ca>
2018-07-06 13:23:35 -0400

    Created function for building tensors and using pretrained VGG16 model to obtain semantic image information

--- style.py
+++ style.py
@@ -11,4 +11,22 @@ from scipy.misc import imsave
 
 from preprocess import *
 
-content_arr, style_arr = process_images('./Images/content.jpg','./Images/style.jpg')
+def make_keras(content_path, style_path)
+    #Defining variables in Keras backend
+    content_arr, style_arr = process_images(content_path,style_path)
+    content_image = backend.variable(content_arr)
+    style_image = backend.variable(style_arr)
+
+    #Temporary placeholder for combination of content and style image
+    combined_image = backend.placeholder((1, 512, 512, 3))
+
+    #Building single tensor containing style, content, and combination images suitable for Keras's VGG16
+    input_tensor = backend.concatenate([content_image, style_image, combined_image], axis=0)
+
+    #Accessing Keras's pretrained VGG16 model, set top to false as we are not concerned with classification
+    model = VGG16(input_tensor=input_tensor, weights='imagenet', include_top=False)
+
+    #Storing layer information in a dictionary
+    layers = dict([(layer.name, layer.output) for layer in model.layers])
+
+    return layers
