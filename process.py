8b8281e66ca3f64c26d33de2c444aaf8ad3b0d75
danieljabbour <jabbourd@mcmaster.ca>
2018-07-06 12:13:20 -0400

    Added gitignore and added rgb subtraction for preprocessing of images

--- process.py
+++ process.py
@@ -20,20 +20,29 @@ def process_images(content_path, style_path):
     style_image = Image.open(style_path)
     style_image = style_image.resize((512,512))
 
-    #Prepare images for numerical processing by adding convenience dimension
-
     #Convert image to numpy array and insert a new axis at position 0
     content_arr = np.asarray(content_image, dtype='float32')
-    content_arr = np.expand_dims(content_arr, axis=0)
-
     style_arr = np.asarray(style_image, dtype='float32')
-    style_arr = np.expand_dims(style_arr, axis=0)
+
+    #Calculate mean RGB value
+    content_rgb_avg = np.mean(content_arr, axis=(0,1))
+    style_rgb_avg = np.mean(style_arr, axis=(0,1))
 
     #Numpy array shape is now 1 x 512 x 512 x 3
+    content_arr = np.expand_dims(content_arr, axis=0)
+    style_arr = np.expand_dims(style_arr, axis=0)
 
-    #Processing images for VGG network
+    #Subtract mean RGB value from content and style arrays
+    content_arr[:, :, :, 0] -= content_rgb_avg[0]
+    content_arr[:, :, :, 1] -= content_rgb_avg[1]
+    content_arr[:, :, :, 2] -= content_rgb_avg[2]
 
-    #Subtract mean RGB value
+    style_arr[:, :, :, 0] -= style_rgb_avg[0]
+    style_arr[:, :, :, 1] -= style_rgb_avg[1]
+    style_arr[:, :, :, 2] -= style_rgb_avg[2]
 
+    #Invert last array dimension to convert from RGB to BGR
+    content_arr = content_arr[:, :, :, ::-1]
+    style_arr = style_arr[:, :, :, ::-1]
 
     
