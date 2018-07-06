42044b5d144f6ca5e5e8e06d674fc224703632c5
danieljabbour <jabbourd@mcmaster.ca>
2018-07-06 13:43:00 -0400

    Added content loss calculating function

--- style.py
+++ style.py
@@ -12,6 +12,7 @@ from scipy.misc import imsave
 from preprocess import *
 
 def make_keras(content_path, style_path)
+
     #Defining variables in Keras backend
     content_arr, style_arr = process_images(content_path,style_path)
     content_image = backend.variable(content_arr)
@@ -30,3 +31,30 @@ def make_keras(content_path, style_path)
     layers = dict([(layer.name, layer.output) for layer in model.layers])
 
     return layers
+
+layers = make_keras('./Images/content.jpg','./Images/style.jpg')
+
+#We are minimizing a loss function consisting of content, style, and total variation hence we define 3 weights
+content_weight = 0.025
+style_weight = 5.0
+total_variation_weight = 1.0
+
+#initialize total loss to 0
+loss = backend.variable(0.)
+
+#Use feature spaces provided by model layers to define loss functions
+
+#We will obtain the content feature from layer block2_conv2 as follows from Johnson et al. (2016)
+
+def content_loss():
+
+    #Obtaining content and combined image features information from appropriate layer
+    content_features = layers['block2_conv2'][0, :, :, :]
+    combined_features = layers['block2_conv2'][2, :, :, :]
+
+    #Computing scaled Euclidean distance between feature representations of the 2 images
+    content_loss = backend.sum(backend.square(combined_features - content_features))
+    loss += content_weight * content_loss)
+
+    return loss
+
