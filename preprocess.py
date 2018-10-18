65194f8f1ab90f003bbf6e70103cb9e085b6605d
Daniel <danieljab7@gmail.com>
2018-10-17 20:43:48 -0400

    Updated preprocess script with layer extraction to modularize stages

--- preprocess.py
+++ preprocess.py
@@ -1,9 +1,15 @@
 from PIL import Image
 import numpy as np
 
-def process_images(content_path, style_path):
-    #This function takes 2 image paths as an argument, one content and one style
+from keras import backend
+from keras.models import Model
+from keras.applications.vgg16 import VGG16
+from keras.applications.vgg16 import preprocess_input
+
+def process_images(content_path, style_path, combined_image):
+    #This function takes 2 image paths as an argument, one content and one style, and a white noise image
     #The images are formated into a numpy array to be used in a TensorFlow Graph
+    #We then extract the appropriate features from a pretrained image classification model
 
     #Loading and resizing images to width x height of 512 x 512
     content_image = Image.open(content_path)
@@ -37,7 +43,17 @@ def process_images(content_path, style_path):
     content_arr = content_arr[:, :, :, ::-1]
     style_arr = style_arr[:, :, :, ::-1]
 
-    return content_arr, style_arr
+    # Pass to tensorflow, use a white noise placeholder image for initial combination
+    content_image = backend.variable(content_arr)
+    style_image = backend.variable(style_arr)
 
-#Sample call
-#content, style = process_images('./Images/content.jpg','./Images/style.jpg')
+    #Building single tensor containing style, content, and combination images suitable for Keras's VGG16
+    input_tensor = backend.concatenate([content_image, style_image, combined_image], axis=0)
+
+    #Accessing Keras's pretrained VGG16 model, set top to false as we are not concerned with classification
+    model = VGG16(input_tensor=input_tensor, weights='imagenet', include_top=False)
+
+    #Storing layer information in a dictionary
+    layers = dict([(layer.name, layer.output) for layer in model.layers])
+
+    return layers
